3cc3704067c577a27247f3e6feca18e6ccaf35b6
Daniil Kamoltsev <niatikcorlk@gmail.com>
2025-02-16 13:20:45 +0300

    java lab1

---
+++ java_labs/lab1.java
@@ -0,0 +1,14 @@
+class lab1 {
+    public static void main(String[] args) {
+        // Начинаем с самой внутренней части
+        double value = Math.sqrt(63);
+                
+        // Двигаемся к внешнему корню
+        for (int i = 60; i >= 3; i -= 3) {
+            value = Math.sqrt(i + value);
+        }
+
+        // Вывод результата
+        System.out.printf("Результат: %.6f%n", value);
+    }    
+}
